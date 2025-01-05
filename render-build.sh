bdd4429ba8d8499c37a510238742eb34404f2ad1
HamsaveniBhaskar <hamsavenibhaskar1@gmail.com>
2025-01-05 13:50:02 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Install dependencies for Node.js
+echo "Installing dependencies..."
+npm install
+
+echo "Dependencies installed successfully."
+
+# Start the server
+echo "Starting the server..."
+npm start
